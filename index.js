0b7b541afe42d4d060fa5e4e5d463ef380c13668
unknown <alenabrahamyan0@gmail.com>
2019-03-03 22:02:42 +0400

    final

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+const svgCaptcha = require('svg-captcha');
 const app = express();
+const upload = multer({dest: './photo'});
+const url_web = "keyglass.herokuapp.com";
+var tiv = 0;
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 
-const nodemailer = require('nodemailer');
+
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -16,29 +23,44 @@ var transporter = nodemailer.createTransport({
 });
 
 
+
 app.set('view engine', 'ejs');
 
 app.use('/home_files', express.static('home_files'));
 app.use('/registration_files', express.static('registration_files'));
 
+//home page
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/registration', (req, res) => {
-    res.render('registration');
+//Create Captcha 
+setInterval(function(){ captchas = svgCaptcha.create(); }, 1000);
+app.get('/captchas', function (req, res) {
+  res.type('svg');
+  res.status(200).send(captchas.data);
 });
 
-app.post('/registration', urlencodedParser, (req, res) => {
-    
-    res.render('registration', {data: req.body, url_code: req.query});
-    console.log(req.body);
+//registration page
+app.get('/registration', (req, res) => {
+    res.render('registration', {url_code: req.query.sea, ctext : captchas.text});
+});
+
+
+
+app.post('/registration', upload.single('file'), (req, res) => {
+
+
+    res.render('registration', {data: req.body, url_code: req.query.sea, ctext : captchas.text});
+    tiv++;
+
+   
     
     var mailOptions = {
         from: 'gevtest98@gmail.com',
-        to: 'zarya@tdsoyuz.ru',
-        subject: 'Новые заказы ',
-        html: '<h1>Новые заказы</h1><p>' + req.body.firstName + '</p><p>' + req.body.lastName + '</p><p>' + req.body.middleName + '</p><p>' + req.body.mail + '</p><p>' + req.body.phone + '</p><p>' + req.body.city + '</p><p>' + req.body.where + '</p><p>' + req.body.article + '</p><p>' + req.body.numberCheck + '</p>'
+        to: 'alen.abrahamyan7@tumo.org',
+        subject: 'Новые' + tiv,
+        html: '<h1>Новые заказы</h1><p>' + req.body.firstName + '</p><p>' + req.body.lastName + '</p><p>' + req.body.middleName + '</p><p>' + req.body.mail + '</p><p>' + req.body.phone + '</p><p>' + req.body.city + '</p><p>' + req.body.where + '</p><p>' + req.body.article + '</p><p>' + req.body.numberCheck + '</p><img src="'+url_web+'/photo/'+req.file.filename+'">'
       };
       
       transporter.sendMail(mailOptions, function(error, info){
@@ -50,6 +72,7 @@ app.post('/registration', urlencodedParser, (req, res) => {
       
       });
 
+      
 });
 
 
